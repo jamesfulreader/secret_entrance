57164a28741d31af930b53a7a03c6c60c4f1bb7b
jamesfulreader <jamesfulreader@gmail.com>
2025-12-01 14:54:53 -0700

    - add .gitignore - read input line by line split direction and clicks - calculate direction of dial

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,7 +19,38 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
+	start := 50
+	end := 0
+	countZero := 0
+
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		rotation := strings.SplitAfterN(scanner.Text(), "", 2)
+		direction := rotation[0]
+		fmt.Printf("Turn %s ", direction)
+		clicks, err := strconv.Atoi(rotation[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%d clicks\n", clicks)
+
+		if direction == "L" {
+			end = start - clicks
+		} else {
+			end = start + clicks
+		}
+		start = end
+
+		if start < 0 {
+			start += 100
+		} else if start > 99 {
+			start -= 100
+		}
+
+		if start == 0 {
+			countZero++
+		}
+
+		fmt.Println("The dial points at", start)
 	}
+	fmt.Printf("In the end the dial pointed at 0 location %d times\n", countZero)
 }
